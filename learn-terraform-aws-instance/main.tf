a57c15a9cfd322b945e219ee6378b0c2376ef2f0
Ryosuke Morita <morita1ryosuke@gmail.com>
2024-03-13 08:39:32 +0900

    AMI が違ったっぽいので東京リージョンの AMI を指定

--- learn-terraform-aws-instance/main.tf
+++ learn-terraform-aws-instance/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-0dfa284c9d7b2adad"
   instance_type = "t2.micro"
 
   tags = {
